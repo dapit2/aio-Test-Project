8891f90a2ddaa1ff8deb128e8297dc2e0857b224
david2 <101044543+lskeren81@users.noreply.github.com>
2025-05-11 07:44:22 +0000

    #

--- commands/example.js
+++
@@ -1,7 +0,0 @@
-export default {
-    name: 'example',
-    pattern: /^!ping/i,
-    execute: async (sock, msg) => {
-      await sock.sendMessage(msg.key.remoteJid, { text: 'Pong!' });
-    }
-  };
